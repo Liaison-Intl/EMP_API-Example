f540048eda794f6582886db8d1c20682ee0ff885
pfela <peter.fela@insidespectrum.com>
2014-11-21 14:09:15 -0500

    Initial commit

---
+++ scripts/config.default.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['SpectrumEMPAPIKey'] = "INSERT_YOUR_SPECTRUM_EMP_API_KEY_HERE";
+
+// these typically don't need to be modified
+define('API_URL', 				'https://www.spectrumemp.com/api/');
+define('REQUIREMENTS_URL', 		API_URL . 'inquiry_form/requirements');
+define('SUBMIT_URL', 			API_URL . 'inquiry_form/submit');
+
+?>
