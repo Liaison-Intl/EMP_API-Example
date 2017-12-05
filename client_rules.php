3d92770aecda6d091d2cf0117a53406054cba098
Chuck Collishaw <ccollishaw@liaison-intl.com>
2017-12-05 20:53:53 +0000

    Remove extra code

--- client_rules.php
+++ client_rules.php
@@ -6,15 +6,8 @@ require_once('scripts/functions.php');
 $url = ALL_CLIENT_RULES_URL . '?CLIENT-ID=' . $config['SpectrumEMPClientID'];
 
 $ch = curl_init($url);
-
-// set the url, number of POST vars, POST data
-curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// execute post
 curl_exec($ch);
-
-// close connection
 curl_close($ch);
 
 ?>
--- field_rules_js.php
+++ field_rules_js.php
@@ -5,15 +5,8 @@ require_once('scripts/config.php');
 $url = WC_URL . 'assets/scripts/plugins/field_rules.js';
 
 $ch = curl_init($url);
-
-// set the url, number of POST vars, POST data
-curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// execute post
 curl_exec($ch);
-
-// close connection
 curl_close($ch);
 
 ?>
